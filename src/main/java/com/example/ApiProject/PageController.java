23b1aa9406b00d176e8f2c54ce6809f933a7b365
Hvilsted <lahvi22@student.sdu.dk>
2024-03-13 01:37:29 +0100

    Progress on GUI

--- src/main/java/com/example/ApiProject/PageController.java
+++ src/main/java/com/example/ApiProject/PageController.java
@@ -2,16 +2,12 @@ package com.example.ApiProject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
 
 @Controller
 public class PageController {
-
-    @GetMapping("/Monitor")
-    @ResponseBody
-    public String Gui() {
-        return "MonitorPage";
+    @GetMapping("/")
+    public String MonitorPage() {
+        return "MonitorPage.html";
     }
 }
+
